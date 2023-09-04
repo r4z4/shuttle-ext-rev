3ea5b61aa88a2cb29c2c7915a357fc6dabafd72c
r4z4 <r4z4aa@gmail.com>
2023-09-04 21:05:56 +0000

    halted due to sqlx

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,11 +1,21 @@
 use axum::{routing::get, Router};
+use sea_orm::{
+    entity::prelude::*, ActiveValue, ConnectOptions, ConnectionTrait, Database, DatabaseConnection,
+    SqlxPostgresConnector, SqlxPostgresPoolConnection,
+};
 
 async fn hello_world() -> &'static str {
     "Hello, world!"
 }
 
 #[shuttle_runtime::main]
-async fn axum() -> shuttle_axum::ShuttleAxum {
+async fn axum(
+    #[shuttle_shared_db::Postgres] pool: PgPool,
+) -> shuttle_axum::ShuttleAxum {
+    let conn = SqlxPostgresConnector::from_sqlx_postgres_pool(pool);
+    // pool.execute(include_str!("../schema.sql"))
+    //     .await
+    //     .map_err(|_error| StatusCode::INTERNAL_SERVER_ERROR)?;
     let router = Router::new().route("/", get(hello_world));
 
     Ok(router.into())
